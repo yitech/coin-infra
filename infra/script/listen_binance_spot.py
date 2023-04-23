53581ad2f635f1687157cd5ce2a70d16be6784de
yitech-owl3d <yite@owl3d.ai>
2023-04-23 09:42:15 +0800

    extract inputs

--- infra/script/listen_binance_spot.py
+++ infra/script/listen_binance_spot.py
@@ -1,24 +1,30 @@
 import asyncio
 import ccxt.async_support as ccxt
+import argparse
 
-async def main():
+async def main(symbol, delay):
     binance = ccxt.binance({
         'enableRateLimit': True,
     })
 
     while True:
         try:
-            ticker = await binance.fetch_ticker('BTC/USDT')
+            ticker = await binance.fetch_ticker(symbol)
             print("Symbol: {}, Bid: {}, Ask: {}".format(
                 ticker['symbol'], ticker['bid'], ticker['ask']
             ))
-            await asyncio.sleep(5)  # Adjust the delay as needed.
+            await asyncio.sleep(delay)  # Adjust the delay as needed.
         except Exception as e:
             print(f"Error: {e}")
-            await asyncio.sleep(5)
+            await asyncio.sleep(delay)
         finally:
             await binance.close()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Monitor the spot price of a trading pair on Binance.")
+    parser.add_argument("symbol", help="Trading pair symbol (e.g., BTC/USDT).")
+    parser.add_argument("delay", type=int, help="Time delay between price fetches in seconds.")
+    args = parser.parse_args()
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(args.symbol, args.delay))
