72f4dd4a4ca199cf4a0b1a9e2bf141f29997af8a
yitech-owl3d <yite@owl3d.ai>
2023-04-27 20:31:34 +0800

    revert binance spot

--- monitor/monitor_binance.py
+++ monitor/monitor_binance.py
@@ -38,4 +38,13 @@ async def main(symbol, delay, output_format, depth):
         finally:
             await binance.close()
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Monitor the spot price of a trading pair on Binance.")
+    parser.add_argument("symbol", help="Trading pair symbol (e.g., BTC/USDT).")
+    parser.add_argument("delay", type=int, help="Time delay between price fetches in seconds.")
+    parser.add_argument("--format", choices=['text', 'json'], default='text', help="Output format: text or json.")
+    parser.add_argument("--depth", type=int, default=5, help="Number of levels to fetch from the order book.")
+    args = parser.parse_args()
 
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args.symbol, args.delay, args.format, args.depth))
