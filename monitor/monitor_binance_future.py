1c8836138cadf08c3441a04cfe92aa7426bf83b1
yitech-owl3d <yite@owl3d.ai>
2023-04-27 20:35:56 +0800

    monitor

--- monitor/monitor_binance_future.py
+++ monitor/monitor_binance_future.py
@@ -1,3 +1,5 @@
+import argparse
+import json
 import ccxt.async_support as ccxt_async
 import asyncio
 import os
@@ -14,19 +16,45 @@ exchange = ccxt_async.binance({
     },
 })
 
-async def print_future_price(symbol):
+async def main(symbol, delay, output_format, depth):
     while True:
         try:
             ticker = await exchange.fetch_ticker(symbol)
-            print(f"{symbol} price: {ticker['last']}")
-            await asyncio.sleep(5)  # Adjust the sleep interval as needed
+            orderbook = await exchange.fetch_order_book(symbol, limit=depth)
+            bids = orderbook['bids']
+            asks = orderbook['asks']
+
+            if output_format == 'text':
+                print(f"{symbol} price: {ticker['last']}")
+                print("Bids:")
+                for bid in bids:
+                    print(f"  Price: {bid[0]}, Amount: {bid[1]}")
+                print("Asks:")
+                for ask in asks:
+                    print(f"  Price: {ask[0]}, Amount: {ask[1]}")
+            elif output_format == 'json':
+                data = {
+                    'symbol': symbol,
+                    'price': ticker['last'],
+                    'bids': bids,
+                    'asks': asks,
+                }
+                print(json.dumps(data))
+
+            await asyncio.sleep(delay)
         except Exception as e:
             print(f"Error: {e}")
-            await asyncio.sleep(60)
+            await asyncio.sleep(10)
 
-symbol = 'BTC/USDT'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Monitor the spot price of a trading pair on Binance.")
+    parser.add_argument("symbol", help="Trading pair symbol (e.g., BTC/USDT).")
+    parser.add_argument("delay", type=int, help="Time delay between price fetches in seconds.")
+    parser.add_argument("--format", choices=['text', 'json'], default='text', help="Output format: text or json.")
+    parser.add_argument("--depth", type=int, default=5, help="Number of levels to fetch from the order book.")
+    args = parser.parse_args()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(print_future_price(symbol))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args.symbol, args.delay, args.format, args.depth))
 
 
