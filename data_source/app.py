483c2394cd60b1b402dbd6ea43a20e8d47874bdf
yite <yite@owl3d.ai>
2023-05-19 13:37:59 +0000

    update one node

--- data_source/app.py
+++ data_source/app.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 import os
 import asyncio
 import aiohttp
@@ -13,46 +13,56 @@ config_path = os.environ.get('CONFIG_PATH')
 with open(config_path, 'r') as f:
     config = json.load(f)
 
-async def fetch(session, url):
-    print(f"Fetching URL: {url}")
+# Kafka Producer
+producer = Producer({'bootstrap.servers': config['kafka_url']})
+
+async def get_url(session, url):
     async with session.get(url) as response:
-        data = await response.json()
-        print(f"Data fetched from {url}: {data}")
-        return data
+        return await response.json()
 
-async def fetch_all(urls, loop):
-    async with aiohttp.ClientSession(loop=loop) as session:
-        results = await asyncio.gather(*[fetch(session, url) for url in urls], return_exceptions=True)
-        return results
-
-def send_to_kafka(data):
-    conf = {'bootstrap.servers': config['kafka_url']}
-    producer = Producer(conf)
-
-    for i, item in enumerate(data):
-        print(f"Sending data_{i} to Kafka: {item}")
-        producer.produce(config['topic'], key=f'data_{i}', value=json.dumps(item))
-
-    print("Data sent to Kafka, waiting for it to be written...")
-    producer.flush()
-    print("Data written to Kafka successfully.")
-
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
 async def main():
-    urls = list(config['endpoints'].values())
-    while True:
-        loop = asyncio.get_event_loop()
-        print("Starting to fetch data from URLs...")
-        htmls = await fetch_all(urls, loop)
-        print("Finished fetching data, sending to Kafka...")
-        send_to_kafka(htmls)
-        print("Sleeping for 1 second before next fetch...")
-        await asyncio.sleep(1)  # Sleep for a second
+    async with aiohttp.ClientSession() as session:
+        #  Schedule the first call to wait(
+        interval = 1 / config['frequency']
+        trigger = (datetime.now() + timedelta(seconds=2)).replace(microsecond=0)
+        wait = datetime.now() - trigger 
+        await asyncio.sleep(wait.total_seconds())
+        metadata = {
+            "_id": uuid.uuid4().hex,
+            "exchange": config["exchange"],
+            "symbol": config["symbol"]
+        }
+        
+        while True:
+            for _ in range(int(config['frequency']) - 1):
+                data = await get_url(session, config['endpoint'])
+                data.update(metadata)
+                await asyncio.sleep(interval)
+                # Trigger any available delivery report callbacks from previous produce() calls
+                producer.poll(0)
 
+                # Asynchronously produce a message, the delivery report callback
+                # will be triggered from poll() above, or flush() below, when the message has
+                # been successfully delivered or failed permanently.
+                producer.produce(config['topic'], key=config['exchange'], value=json.dumps(data).encode('utf-8'), callback=delivery_report)
+
+                # Wait for any outstanding messages to be delivered and delivery reports
+                # to be triggered.
+                producer.flush()
+            trigger = (datetime.now() + timedelta(seconds=1)).replace(microsecond=0)
+            wait = datetime.now() - trigger 
+            # Wait for the next call to wait()
+            await asyncio.sleep(wait.total_seconds())
+            
+
+# Run the main function
 loop = asyncio.get_event_loop()
-try:
-    print("Starting main loop...")
-    loop.run_until_complete(main())
-finally:
-    print("Closing main loop...")
-    loop.close()
+loop.run_until_complete(main())
