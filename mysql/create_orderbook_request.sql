24b8b3b2410e9121d7664bfeb21b77b17afd61fe
yite-coin-project-1 <coastq22889@icloud.com>
2023-05-21 09:35:20 +0000

    remove mysql

--- mysql/create_orderbook_request.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE OrderBookRequest (
-    request_id INT AUTO_INCREMENT,
-    request_time TIMESTAMP,
-    response_time TIMESTAMP,
-    exchange VARCHAR(20),
-    symbol VARCHAR(20),
-    PRIMARY KEY (request_id)
-);
