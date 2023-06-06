6e28bdba984d840f839d77af00d4fd32165ff5eb
yite <coastq22889@icloud.com>
2023-06-06 01:01:08 +0000

    update recording script

--- run.sh
+++ run.sh
@@ -3,26 +3,24 @@
 set -xe
 
 # Validate command-line arguments
-if [ $# -ne 3 ]; then
-  echo "Usage: ./run.sh <exchange> <symbol> <type>"
+if [ $# -ne 2 ]; then
+  echo "Usage: ./run.sh <python_script> <config_file>"
   exit 1
 fi
 
-# Assign arguments to variables
-exchange=$1
-symbol=$2
-type=$3
+# Get arguments
+python_script=$1
+config_file=$2
 
-# Get the current date and time
-timestamp=$(date +'%Y_%m_%d_%H_%M_%S')
+# Get the base name of the config file without extension
+config_file_base=$(basename $config_file .json)
 
 # Construct filenames
-log_file="log/${exchange}_${symbol}_${type}_${timestamp}.log"
-config_file="configuration/config_${exchange}_${symbol}_${type}.json"
-python_script="ws_${exchange}_100ms_entry.py"
+timestamp=$(date +"%Y%m%d%H%M%S")
+log_file="artifact/log/${config_file_base}_${timestamp}.log"
 
 # Set the Python executable path
 python_exec="$(pwd)/venv/bin/python"
 
 # Execute the command
-nohup $python_exec $python_script --config $config_file >> $log_file 2>&1 &
+nohup $python_exec $python_script $config_file >> $log_file 2>&1 &
---
+++ script/record_binance.py
@@ -0,0 +1,62 @@
+import os
+import argparse
+import asyncio
+import logging
+import websockets
+import json
+from datetime import datetime, timezone
+import uuid
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+async def dump_to_file(outdir, symbol, batch_size, queue):
+    batch = []
+    while True:
+        while len(batch) < batch_size:
+            data = await queue.get()
+            batch.append(data)
+        formatted = datetime.now().strftime("%Y%m%d%H")
+        filename = f"{symbol.replace('/', '_')}_{formatted}.txt"
+        outfile = os.path.join(outdir, filename)
+        with open(outfile, 'a') as f:
+            for data in batch:
+                json.dump(data, f)
+                f.write('\n')
+            batch.clear()
+
+async def main(args):
+    queue = asyncio.Queue()
+    WS_ADDRESS = args["websocket_url"]
+    try:
+        async with websockets.connect(WS_ADDRESS) as websocket:
+            dump_task = asyncio.create_task(dump_to_file(args["outdir"], args["symbol"], args["batch_size"], queue))
+            async for message in websocket:
+                try:
+                    json_data = json.loads(message)
+                    dt_object = datetime.fromtimestamp(json_data["T"] / 1000, timezone.utc)
+                    json_data = {"id": uuid.uuid4().hex, 
+                                 "symbol": args["symbol"], 
+                                 "timestamp": dt_object.isoformat(), 
+                                 'ask': [[float(item) for item in sublist] for sublist in json_data['a']], 
+                                 'bid': [[float(item) for item in sublist] for sublist in json_data['b']]}
+                except json.JSONDecodeError:
+                    logging.error('JSONDecodeError for message: %s', message)
+                    continue
+                await queue.put(json_data)
+    except KeyboardInterrupt:
+        # Final dump when KeyboardInterrupt is raised
+        await dump_task
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Websocket reader and json dumper.')
+    parser.add_argument('json_file', type=str, help='Json file containing arguments')
+    args = parser.parse_args()
+
+    with open(args.json_file, 'r') as f:
+        json_args = json.load(f)
+
+    asyncio.run(main(json_args))
