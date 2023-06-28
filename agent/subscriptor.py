3c9ccf5dfec8b6082d7416962189553a5053bb67
yite <coastq22889@icloud.com>
2023-06-28 16:53:15 +0000

    commit to queue

--- agent/subscriptor.py
+++ agent/subscriptor.py
@@ -1,45 +1,52 @@
 import argparse
 import json
-import redis
+import aioredis
+import asyncio
 import traceback
 from urllib.parse import urlparse
-from pymongo import MongoClient
-from queue import Queue
+from motor.motor_asyncio import AsyncIOMotorClient
 from coin_infra.core import Logger
 
 class Consumer:
     def __init__(self, redis_url, channel, mongo_url):
-        parsed = urlparse(redis_url)
-        host = parsed.hostname
-        port = parsed.port
-        self.redis_conn = redis.Redis(host, port)
+        self.redis_url = redis_url
         self.channel = channel
-
-        parsed = urlparse(mongo_url)
-        protocol_host = parsed.scheme + "://" + parsed.hostname
-        port = parsed.port
-        self.mongo_client = MongoClient(protocol_host, port)
-
+        self.mongo_url = mongo_url
+        self.queue = asyncio.Queue()
         self.logger = Logger(__name__)
 
-    def consume(self):
+    async def init_connections(self):
+        self.redis_conn = await aioredis.from_url(self.redis_url)
+        self.mongo_client = AsyncIOMotorClient(self.mongo_url)
+
+    async def consume(self):
         try:
+            await self.init_connections()
+
             pubsub = self.redis_conn.pubsub()
-            pubsub.subscribe(self.channel)
-            
+            channel = await pubsub.subscribe(self.channel)
+            self.logger.info(f'Subscribed to {self.channel}')
+
             # Skip the first message (the subscription confirmation)
-            confirmation =  next(pubsub.listen())
+            confirmation = await pubsub.get_message()
             self.logger.info(f'Confirmation: {confirmation}')
 
-            for message in pubsub.listen():
-                data = json.loads(message['data'].decode('utf-8'))
-                self.logger.info(f'{data=}')
-                
-                
+            while True:
+                message = await pubsub.get_message(ignore_subscribe_messages=True)
+                if message:
+                    data = json.loads(message['data'].decode('utf-8'))
+                    self.logger.info(f'{data=}, {self.queue.qsize()}')
+                    await self.queue.put(data)  # Put data in queue
+                else:
+                    await asyncio.sleep(1)  # wait for a second if no message
+
         except Exception as e:
-            self.logger.info(f'Error occurred: {traceback.format_exc()}')
+            self.logger.error(f'Error occurred: {traceback.format_exc()}')
+
         finally:
-            pubsub.close()
+            await pubsub.unsubscribe(self.channel)
+            self.redis_conn.close()
+            await self.redis_conn.wait_closed()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Binancefutures orderbook reader')
@@ -53,5 +60,4 @@ if __name__ == "__main__":
     mongo = json_args['mongo']
 
     sub = Consumer(broker['url'], broker['channel'], mongo['url'])
-    sub.consume()
-
+    asyncio.run(sub.consume())
