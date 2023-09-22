964201aedfd175890c7a6adcc50d7ddc6cbd8eeb
yite-orichal <yite@orichal.io>
2023-09-22 12:40:27 +0000

    redis_queue_handler

---
+++ general/queue/redis_queue_hander.py
@@ -0,0 +1,19 @@
+import os
+import json
+import redis
+
+
+class RedisQueueHandler:
+    def __init__(self, host, port, queue_name):
+        self.redis_conn = redis.Redis(host=host, port=port, db=0)
+        self.queue_name = queue_name
+
+    def enqueue(self, data):
+        self.redis_conn.rpush(self.queue_name, json.dumps(data))
+
+    def dequeue(self):
+        _, msg = self.redis_conn.blpop(self.queue_name)
+        return json.loads(msg)
+
+    def close(self):
+        del self.redis_conn
