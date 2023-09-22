7a95756b19c6a87f2854c648df0449466c2ce50c
yite-orichal <yite@orichal.io>
2023-09-22 12:43:40 +0000

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='general',
-    version='1.1',
+    version='2.0',
     description='infra of coin projects',
     author='Yi Te',
     author_email='coastq22889@icloud.com',
