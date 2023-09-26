4c9734a36122ac8a0403f10aa7ac57e14f632da8
yite-orichal <yite@orichal.io>
2023-09-26 12:51:41 +0000

    binanceum restful

--- general/exchange/binance_um/binanceum_handler.py
+++ general/exchange/binance_um/binanceum_handler.py
@@ -1,14 +1,26 @@
 from general.exchange.type_enum import BINANCEUM
 from general.exchange.exchange_handler import ExchangeHandler
 from binance.um_futures import UMFutures
+from general.exchange.type_enum import (
+    BUY, SELL, MARKET, LIMIT,
+    GTC, IOC, FOK, POST_ONLY
+)
 
 
 class BinanceUMHandler(ExchangeHandler):
     NAME = BINANCEUM
 
     def __init__(self, api_key='', api_secret=''):
-        if api_key and api_secret:
-            self.um_futures = UMFutures(key=api_key, secret=api_secret)
+        self.um_futures = UMFutures(key=api_key, secret=api_secret)
+
+    def create_market_order(self, symbol, side, qty):
+        ret = self.um_futures.new_order(symbol=symbol, side=side, type=MARKET, quantity=qty)
+        return ret
+
+    def create_limit_order(self, symbol, side, qty, price, time_in_force=GTC):
+        ret = self.um_futures.new_order(symbol=symbol, side=side, type=LIMIT, quantity=qty, price=price,
+                                        timeInForce=time_in_force)
+        return ret
 
     @staticmethod
     def to_market_price(data):
--- general/exchange/type_enum.py
+++ general/exchange/type_enum.py
@@ -1,2 +1,13 @@
 BINANCEUM = 'BinanceUM'
 OKX = 'OKX'
+
+BUY = 'buy'
+SELL = 'sell'
+
+MARKET = 'market'
+LIMIT = 'limit'
+
+GTC = 'GTC'
+FOK = 'FOK'
+IOC = 'IOC'
+POST_ONLY = 'PostOnly'
