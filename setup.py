a456aed67c767ac95aa56c741a1e5922fefcfac0
yite-orichal <yite@orichal.io>
2023-10-19 15:33:11 +0000

    multithread

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='coin-infra',
-    version='4.1.2',
+    version='4.2.0',
     description='infra of coin projects',
     author='Yi Te',
     author_email='coastq22889@icloud.com',
-    packages=find_packages(),
+    packages=find_packages(include=['general', 'general.*']),
     install_requires=[
         'pymongo',
         'redis',
---
+++ test/multithread/test_threadpool.py
@@ -0,0 +1,38 @@
+import unittest
+import time
+from general.mutithread import ThreadPool
+
+
+class TestThreadPool(unittest.TestCase):
+    def test_add_function(self):
+        results = []
+
+        # Job to add two numbers and append the result to the results list
+        def add_and_store(a, b):
+            result = a + b
+            results.append(result)
+
+        # Initialize thread pool with 4 workers
+        pool = ThreadPool(4)
+
+        # Data for testing
+        test_data = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6),
+                     (6, 7), (7, 8), (8, 9), (9, 10), (10, 11)]
+
+        # Add jobs to the thread pool using the test data
+        for data in test_data:
+            pool.add_job(add_and_store, data)
+
+        # Wait for all jobs to complete
+        pool.wait_completion()
+
+        # Check that all jobs were executed
+        self.assertEqual(len(results), 10)
+
+        # Validate the results of the addition
+        expected_results = [sum(data) for data in test_data]
+        self.assertEqual(sorted(results), expected_results)
+
+
+if __name__ == "__main__":
+    unittest.main()
